2faf60b1c4ded7ecfd7abd4e5a7f814e0cef9bfa
Memo <hyperhorus@gmail.com>
2022-04-24 11:29:28 -0500

    Added PPOST, PUT, PATCH, DELETE metthods in AppView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializer
 
 class HelloApiView(APIView):
     """Test APIView"""
+    serializer_class = serializer.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of APIView features"""
@@ -14,3 +19,43 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name """    
+        serializer = self.serializer_class(data=request.data)
+       
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors, 
+                status= status.HTTP_400_BAD_REQUEST               
+                )
+
+    def put(self, request, pk=None):
+        """Method PUT"""
+        return Response({'message':'Put your ligths on'})
+
+    def patch(self, request, pk=None):
+        """Method PATCH"""
+        return Response({'message':'Patch your ass on'})  
+
+    def delete(self, request, pk=None):
+        """Method DELETE"""
+        return Response({'message':'Delete my number!!'})      
+
+
+class OtraPrueba(APIView):
+    """Una prueba"""
+
+    def get(self, request, format=None):
+        """Regresa otra mamada"""
+        una_lista_x = [
+            'esta es la lista',
+            'de lo que se prueba',
+            'en esta pagina'
+        ]
+
+        return Response({'message': 'Me cachis', 'una_lista_x': una_lista_x})
